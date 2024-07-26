fa28d88815d72fdcb2ec2164b1fbbeb7371a1fdc
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-26 21:07:23 +0200

    creazione ciclo for

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-console.log('ciao mondo')
-
-
 //array di oggetti cioè le immagini con relativi titoli e descizione
 const images = [
     {
@@ -25,3 +22,22 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+//creo una variabile globale che utilizzerò per inserire le immagini
+let items = '';
+//creo un ciclo per prendermi le immagini con titolo e descizione E inserirle poi nel HTML
+for (let i = 0; i < images.length; i++) {
+    if (i === 0) {
+        items += `<li class="carousel-slide active">`;
+    } else {
+        items += `<li class="carousel-slide">`;
+    }
+    items += `
+        <img src="${images[i].image}" alt="${images[i].title}">
+        <div class="carousel-caption">
+            <h3>${images[i].title}</h3>
+            <p>${images[i].text}</p>
+        </div>
+    </li>`;
+    //salvo in console per vedere 
+    console.log('items', items, typeof items);
+};
